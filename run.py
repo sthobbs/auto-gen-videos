35b7862453a431773cfc34b76e414115be0d9103
Steve Hobbs <hobbs_steve@hotmail.com>
2023-01-03 00:19:11 -0500

    lint run script

--- run.py
+++ run.py
@@ -7,7 +7,7 @@ os.chdir(pwd)
 import pandas as pd
 from pathlib import Path
 from scrape import TripAdvisorScrape
-from bing_image_downloader import downloader # note that I modified this package
+from bing_image_downloader import downloader  # modified original code
 from enhance_image import Enhance
 from gen_video import Video
 
@@ -15,14 +15,32 @@ from gen_video import Video
 attractions_dir = 'attractions'
 image_dir = 'images'
 enhanced_subdir = 'enhance'
-image_size = 'wallpaper' # TODO: increase image size later
+image_size = 'wallpaper'
 video_dir = 'videos'
 audio_dir = 'audio'
 
 
 def make_dir(dir):
     if not os.path.exists(dir):
-        os.makedirs(dir) 
+        os.makedirs(dir)
+
+
+def image_download(query, extra_query, output_dir, image_size='medium'):
+    assert image_size in ['small', 'medium', 'large', 'wallpaper'], \
+        "image_size is must be 'samll', 'medium', 'large', or 'wallpaper'"
+    filters = f"+filterui:aspect-wide+filterui:license-L1+filterui:imagesize-{image_size}"
+    downloader.download(query, extra_query=extra_query, limit=1,
+                        output_dir=output_dir, adult_filter_off=False,
+                        force_replace=False, timeout=60, filters=filters,
+                        query_folder=False)
+
+
+# custom sort to order attractions by index in df
+def sort_attractions(path, attractions):
+    # get file name after removing the extension
+    attr = '.'.join(Path(path).name.split('.')[:-1])
+    all_attractions = list(attractions['Attraction'])
+    return all_attractions.index(attr)
 
 
 # Web scraping prep
@@ -32,20 +50,8 @@ scraper = TripAdvisorScrape()
 # image scrape prep
 make_dir(image_dir)
 
-def image_download(query, extra_query, output_dir, image_size='medium'):
-    assert image_size in ['small', 'medium', 'large', 'wallpaper'], f"{image_size} is must be one of 'samll', 'medium', 'large', or 'wallpaper'"
-    filters = f"+filterui:aspect-wide+filterui:license-L1+filterui:imagesize-{image_size}"
-    downloader.download(query, extra_query=extra_query, limit=1, output_dir=output_dir, adult_filter_off=False, force_replace=False, timeout=60, filters=filters, query_folder=False)
-
 # video generation prep
 make_dir(video_dir)
-
-# custom sort to order attractions by index in df
-def sort_attractions(path, attractions):
-    attr = '.'.join(Path(path).name.split('.')[:-1]) # file name after removing the .jpg or other extension
-    all_attractions = list(attractions['Attraction'])
-    return all_attractions.index(attr)
-
 
 locations = pd.read_csv("locations.csv", encoding='cp1252')
 
@@ -72,14 +78,15 @@ for i in range(len(locations)):
     # download an image for each attraction
     if locations.loc[i, 'To Image'] == 'yes' and locations.loc[i, 'Imaged'] != 'yes':
         print(f"getting images for {loc}")
-        errors = False 
+        errors = False
         attractions = pd.read_csv(f"{attractions_dir}\\{loc}.csv", encoding='cp1252')
         for j in range(len(attractions)):
             attr = attractions.loc[j, 'Attraction']
             try:
+                # overwrite images with higher quality ones if they exist
                 image_download(attr, loc, f"{image_dir}\\{loc}", image_size='medium')
                 image_download(attr, loc, f"{image_dir}\\{loc}", image_size='large')
-                image_download(attr, loc, f"{image_dir}\\{loc}", image_size='wallpaper') # overwrite images with higher quality ones 
+                image_download(attr, loc, f"{image_dir}\\{loc}", image_size='wallpaper')
             except Exception as e:
                 print(e)
                 errors = True
@@ -87,7 +94,6 @@ for i in range(len(locations)):
             locations.loc[i, 'Image Error'] = 'yes'
         else:
             locations.loc[i, 'Imaged'] = 'yes'
-
 
     # enhance images
     if locations.loc[i, 'To Enhance'] == 'yes' and locations.loc[i, 'Enhanced'] != 'yes':
@@ -109,16 +115,18 @@ for i in range(len(locations)):
             # get all attractions to sort image_paths by attraction rank
             attractions = pd.read_csv(f"{attractions_dir}\\{loc}.csv", encoding='cp1252')
             # find attractions that we have images for
+            # (sometimes use enhanced_dir = f"{image_dir}\\{loc}")
             enhanced_dir = f"{image_dir}\\{loc}\\{enhanced_subdir}"
-            #enhanced_dir = f"{image_dir}\\{loc}"
-            image_paths = [f"{enhanced_dir}\\{file}" for file in os.listdir(enhanced_dir) if os.path.isfile(f"{enhanced_dir}\\{file}")]
+            image_paths = [f"{enhanced_dir}\\{file}" for file in os.listdir(enhanced_dir)
+                           if os.path.isfile(f"{enhanced_dir}\\{file}")]
             # sort image_paths
             image_paths.sort(key=lambda path: sort_attractions(path, attractions))
             # take top x paths where x is rounded to the nearest 5
-            image_paths = image_paths[:(len(image_paths) - len(image_paths)%5)]
+            image_paths = image_paths[: (len(image_paths) - len(image_paths) % 5)]
             # generate video
-            video = Video(image_paths=image_paths, output_dir=f"{video_dir}\\{loc}", audio_dir=audio_dir, resolution='4K', fps=60)
-            video.gen_thumbnails(resolution='4K', sub_dir='4K') #title='DENVER')
+            video = Video(image_paths=image_paths, output_dir=f"{video_dir}\\{loc}",
+                          audio_dir=audio_dir, resolution='4K', fps=60)
+            video.gen_thumbnails(resolution='4K', sub_dir='4K')  # title='DENVER')
             video.gen_thumbnails(resolution='QHD', sub_dir='QHD')
             video.gen_thumbnails(resolution='FHD', sub_dir='FHD')
             video.gen_thumbnails(resolution='HD', sub_dir='HD')
@@ -129,9 +137,5 @@ for i in range(len(locations)):
             print(e)
             locations.loc[i, 'Video Error'] = 'yes'
 
-
 # Update locations manager
 locations.to_csv("locations.csv", index=False, encoding='cp1252')
-
-
-
