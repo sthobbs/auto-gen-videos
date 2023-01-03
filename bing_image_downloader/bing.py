6e45c7f4874717497f6fa5692573147026ff15bc
Steve Hobbs <hobbs_steve@hotmail.com>
2023-01-03 00:23:04 -0500

    clean up bing image downloader

--- bing_image_downloader/bing.py
+++ bing_image_downloader/bing.py
@@ -1,6 +1,4 @@
-from pathlib import Path
 import os
-import sys
 import urllib.request
 import urllib
 import imghdr
@@ -8,39 +6,42 @@ import posixpath
 import re
 import time
 
-'''
+"""
 Python api to download image form Bing.
-Author: Guru Prasad (g.gaurav541@gmail.com)
-'''
+Original Author: Guru Prasad (g.gaurav541@gmail.com)
+Modified by: Steve Hobbs (github.com/sthobbs)
+"""
 
 
 class Bing:
-    def __init__(self, query, limit, output_dir, adult, timeout, filters='', query_folder=True, extra_query=''):
-        self.download_count = 0
+    def __init__(self, query, limit, output_dir, adult, timeout, filters='',
+                 query_folder=True, extra_query=''):
         self.query = query
+        self.limit = limit
         self.output_dir = output_dir
         self.adult = adult
+        self.timeout = timeout
         self.filters = filters
-        self.query_folder = query_folder # put all images into a folder based on the query
-        if extra_query != '': # add in extra query words that don't impact output location
+        self.query_folder = query_folder  # folder to put all images into
+        # add in extra query words that don't impact output location
+        if extra_query != '':
             self.extra_query = ' ' + extra_query
         else:
-            self.extra_query = extra_query 
-
-        assert not (not query_folder and limit != 1), "if query_folder==False, then limit must be 1"
-        assert type(limit) == int, "limit must be integer"
-        self.limit = limit
-        assert type(timeout) == int, "timeout must be integer"
-        self.timeout = timeout
-
+            self.extra_query = extra_query
+        self.download_count = 0
         self.headers = {'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:87.0) Gecko/20100101 Firefox/87.0'}
         self.page_counter = 1
+
+        assert not (not query_folder and limit != 1), \
+            "if query_folder==False, then limit must be 1"
+        assert type(limit) == int, "limit must be integer"
+        assert type(timeout) == int, "timeout must be integer"
 
     def save_image(self, link, file_path):
         request = urllib.request.Request(link, None, self.headers)
         image = urllib.request.urlopen(request, timeout=self.timeout).read()
         if not imghdr.what(None, image):
-            print('[Error]Invalid image, not saving {}\n'.format(link))
+            print(f'[Error]Invalid image, not saving {link}\n')
             raise
         with open(file_path, 'wb') as f:
             f.write(image)
@@ -53,31 +54,38 @@ class Bing:
             path = urllib.parse.urlsplit(link).path
             filename = posixpath.basename(path).split('?')[0]
             file_type = filename.split(".")[-1]
-            if file_type.lower() not in ["jpe", "jpeg", "jfif", "exif", "tiff", "gif", "bmp", "png", "webp", "jpg"]:
+            file_types = {
+                "jpe", "jpeg", "jfif", "exif", "tiff",
+                "gif", "bmp", "png", "webp", "jpg"
+            }
+            if file_type.lower() not in file_types:
                 file_type = "jpg"
 
             # Download the image
-            print("[%] Downloading Image #{} from {}".format(self.download_count, link))
-
+            print(f"[%] Downloading Image #{self.download_count} from {link}")
             if self.query_folder:
-                self.save_image(link, "{}\\{}\\{}\\".format(os.getcwd(), self.output_dir, self.query) + "Image_{}.{}".format(
-                    str(self.download_count), file_type))
+                out_dir = f"{os.getcwd()}\\{self.output_dir}\\{self.query}"
+                out_path = f"{out_dir}\\Image_{self.download_count}.{file_type}"
             else:
-                self.save_image(link, "{}\\{}\\".format(os.getcwd(), self.output_dir) + "{}.{}".format(
-                self.query, file_type))
+                out_path = f"{os.getcwd()}\\{self.output_dir}\\{self.query}.{file_type}"
+            self.save_image(link, out_path)
             print("[%] File Downloaded !\n")
         except Exception as e:
             self.download_count -= 1
-            print("[!] Issue getting: {}\n[!] Error:: {}".format(link, e))
+            print(f"[!] Issue getting: {link}\n[!] Error:: {e}")
 
     def run(self):
         while self.download_count < self.limit and self.page_counter < 2:
-            time.sleep(1) # Sleep for 1 second
-            print('\n\n[!!]Indexing page: {}\n'.format(self.page_counter))
+            time.sleep(1)  # Sleep for 1 second
+            print(f'\n\n[!!]Indexing page: {self.page_counter}\n')
             # Parse the page source and download pics
-            request_url = 'https://www.bing.com/images/search?q=' + urllib.parse.quote_plus(self.query + self.extra_query) + '&form=IRFLTR' \
-                          + '&first=' + str(self.page_counter) + '&count=' + str(self.limit) \
-                          + '&adlt=' + self.adult + '&qft=' + self.filters #+ '&tsc=ImageBasicHover'
+            request_url = 'https://www.bing.com/images/search?q=' \
+                          + urllib.parse.quote_plus(self.query + self.extra_query) \
+                          + '&form=IRFLTR' \
+                          + '&first=' + str(self.page_counter) \
+                          + '&count=' + str(self.limit) \
+                          + '&adlt=' + self.adult \
+                          + '&qft=' + self.filters  # + '&tsc=ImageBasicHover'
             print(self.filters)
             print('Request URL: ' + request_url)
             request = urllib.request.Request(request_url, None, headers=self.headers)
@@ -85,15 +93,15 @@ class Bing:
             html = response.read().decode('utf8')
             links = re.findall('murl&quot;:&quot;(.*?)&quot;', html)
 
-            print("[%] Indexed {} Images on Page {}.".format(len(links), self.page_counter))
+            print(f"[%] Indexed {len(links)} Images on Page {self.page_counter}.")
             print("\n===============================================\n")
 
             for link in links:
                 if self.download_count < self.limit:
                     self.download_image(link)
                 else:
-                    print("\n\n[%] Done. Downloaded {} images.".format(self.download_count))
+                    print(f"\n\n[%] Done. Downloaded {self.download_count} images.")
                     print("\n===============================================\n")
                     break
-                time.sleep(1) # Sleep for 1 second
+                time.sleep(1)  # Sleep for 1 second
             self.page_counter += 1
--- bing_image_downloader/downloader.py
+++ bing_image_downloader/downloader.py
@@ -7,13 +7,11 @@ except ImportError:  # Python 3
     from .bing import Bing
 
 
-def download(query, limit=100, output_dir='dataset', adult_filter_off=True, force_replace=False, timeout=60, filters='', query_folder=True, extra_query=''):
+def download(query, limit=100, output_dir='dataset', adult_filter_off=True,
+             force_replace=False, timeout=60, filters='', query_folder=True,
+             extra_query=''):
 
-    # engine = 'bing'
-    if adult_filter_off:
-        adult = 'off'
-    else:
-        adult = 'on'
+    adult = 'off' if adult_filter_off else 'on'
 
     cwd = os.getcwd()
     image_dir = os.path.join(cwd, output_dir, query)
@@ -24,19 +22,23 @@ def download(query, limit=100, output_dir='dataset', adult_filter_off=True, forc
 
     # check directory and create if necessary
     try:
-        if not os.path.isdir("{}\\{}\\".format(cwd, output_dir)):
-            os.makedirs("{}\\{}\\".format(cwd, output_dir))
+        path = f"{cwd}\\{output_dir}\\"
+        if not os.path.isdir(path):
+            os.makedirs(path)
     except:
         pass
-    
-    assert not (not query_folder and limit != 1), "if query_folder==False, then limit must be 1"
-    
-    if query_folder and not os.path.isdir("{}\\{}\\{}".format(cwd, output_dir, query)):
-        os.makedirs("{}\\{}\\{}".format(cwd, output_dir, query))
 
-    bing = Bing(query, limit, output_dir, adult, timeout, filters, query_folder, extra_query)
+    assert not (not query_folder and limit != 1), \
+        "if query_folder==False, then limit must be 1"
+
+    path = f"{cwd}\\{output_dir}\\{query}"
+    if query_folder and not os.path.isdir(path):
+        os.makedirs(path)
+
+    bing = Bing(query, limit, output_dir, adult, timeout, filters,
+                query_folder, extra_query)
     bing.run()
 
 
 if __name__ == '__main__':
-    download('abitabh', limit=10, timeout='1')
+    download('cars', limit=10, timeout='1')
